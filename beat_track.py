3a2a8a05709b0b355b91529b68a2dfba90b77ae7
wass <wassim.filali@gmail.com>
2020-11-21 20:21:46 +0100

    renaming to beat_track

--- beat_track.py
+++ beat_track.py
@@ -2,10 +2,10 @@ import librosa
 
 filename = librosa.example('nutcracker')
 audio, sample_rate = librosa.load(filename)
+print(sample_rate)
 
 tempo, beat_frames = librosa.beat.beat_track(y=audio, sr=sample_rate)
+beat_times = librosa.frames_to_time(beat_frames, sr=sample_rate)
 
 print('tempo: {:.1f} beats per minute'.format(tempo))
-
-beat_times = librosa.frames_to_time(beat_frames, sr=sample_rate)
 print(beat_times)
